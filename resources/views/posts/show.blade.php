066ed2531fc9038b93112cd61e0e0ca68f5aa952
Alisher <ur.alisher@mail.ru>
2018-07-20 07:35:10 +0600

    show post page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,10 +1,30 @@
-@extends('layouts.master') @section('content')
-<h1>{{ $post->title }}</h1>
+@extends('layouts.master')
 
-<div class="panel panel-default">
+@section('content')
+<div class="grey-row">
+  <div class="container height">
+    <div class="news">
+      <div class="row">
+        
+        <div class="col-lg-9 col-md-9 order-first order-md-first order-sm-last order-xs-last news-block">
+          <h4 class="show-news-title">{{ $post->title }}</h4>
+          <span class="one-news-info">Дата: {{ $post->created_at->format('d.m.Y') }}</span>
+          <span class="one-news-info">Просмотров: {{ $post->views }}</span>
 
-  <div class="panel-body">
-    {{ $post->content }}
+          <div class="news-content-block">
+
+            <div class="panel-body">
+              {!! $post->content !!}
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-3 col-md-3 order-last order-md-last order-sm-first order-xs-first pinned-news">
+            <h4 class="title">{{ trans('app.info.pinned') }}</h4>
+            <hr>
+          </div>
+      </div>
+    </div>
   </div>
 </div>
 @endsection
